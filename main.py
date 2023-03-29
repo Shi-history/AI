d346644c9c3ae6f30aa5786a78fcf0ab5640984a
wcventure <wencheng888888@163.com>
2023-03-30 04:10:12 +0800

    false

--- main.py
+++ main.py
@@ -110,4 +110,4 @@ def auto_opentab_delay():
 
 auto_opentab_delay()
 demo.title = "ChatGPT 学术优化"
-demo.queue(concurrency_count=CONCURRENT_COUNT).launch(server_name="0.0.0.0", share=FALSE, server_port=PORT, auth=AUTHENTICATION)
+demo.queue(concurrency_count=CONCURRENT_COUNT).launch(server_name="0.0.0.0", share=False, server_port=PORT, auth=AUTHENTICATION)
